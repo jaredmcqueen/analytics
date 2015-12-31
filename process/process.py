3030b4fab371a23501dc6e2675fb8a8407d73e7c
Brian Brurok <udacity.brian@gmail.com>
2015-12-31 08:10:27 -0500

    writing to epoch time in "seconds"

--- process/process.py
+++ process/process.py
@@ -14,12 +14,12 @@ import os
 import glob # necessary if using csv matching instead of download of open source feeds
 
 
-#### Specify data paths for matching with threat intel and output file for enriched data set
-data = 'raw' + os.sep + 'data.csv'  #Raw network data set
-output ='..' + os.sep + 'examples' + os.sep + 'output.csv'  #output directory for enriched csv - DO NOT CHANGE
+# Specify data paths for matching with threat intel and output file for enriched data set
+data = 'raw' + os.sep + 'data.csv'  # Raw network data set
+output ='..' + os.sep + 'examples' + os.sep + 'output.csv'  # output directory for enriched csv - DO NOT CHANGE
 
 
-#### Identify and collect threat intel feeds
+# Identify and collect threat intel feeds
 def threat_collect():
 
     # Includes Malc0de, emerging threats and Zeus Tracker as examples
@@ -28,7 +28,7 @@ def threat_collect():
     url_zeus = 'https://zeustracker.abuse.ch/blocklist.php?download=ipblocklist'
     url_zeus_domains = 'https://zeustracker.abuse.ch/blocklist.php?download=domainblocklist'
 
-    #Convert to DataFrames
+    # Convert to DataFrames
     df_malc0de = pd.read_table(url_malc0de, index_col=None, skiprows=4, header=None, names=['actor'])
     df_et = pd.read_table(url_et, index_col=None, skiprows=0, header=None, names=['actor'])
     df_zeus = pd.read_table(url_zeus, index_col=None, skiprows=6, header=None, names=['actor'])
@@ -52,14 +52,15 @@ def threat_collect():
     return ti_combine
 
 
-#### Take combined threat intel lists and match against raw dataset. Return properly formatted dataframe.
+# Take combined threat intel lists and match against raw dataset. Return properly formatted dataframe.
 def threat_matcher():
 
     # grab combined threat intel
     ti_combine = threat_collect()
 
     # Read raw network data
-    df_data = pd.read_csv(data, error_bad_lines=False, keep_default_na=False, na_values=[''], header=0, names=['datetime','source', 'target'], parse_dates=True)
+    df_data = pd.read_csv(data, error_bad_lines=False, keep_default_na=False, na_values=[''], header=0,
+                          names=['datetime','source', 'target'], parse_dates=True)
 
     # Find and combine hits
     ti_src = pd.merge(left=df_data, right=ti_combine, left_on='source', right_on='actor')
@@ -67,7 +68,8 @@ def threat_matcher():
     hits_combine = pd.concat([ti_src, ti_tgt], axis=0)
 
     # Merge or Concatenate TI hits and raw data. Using left join to ensure raw dataset remains and hits are "merged"
-    enriched = pd.merge(left=df_data,right=hits_combine, how='left', left_on=['datetime','source', 'target'], right_on=['datetime','source', 'target'])
+    enriched = pd.merge(left=df_data,right=hits_combine, how='left', left_on=['datetime','source', 'target'],
+                        right_on=['datetime','source', 'target'])
 
     # Add columns for src and tgt hits, clean up dataframe and format date time column as a datetime object
     # Next two lines add a column called src_hit or target_hit based on which field matched the indicator
@@ -86,7 +88,7 @@ def threat_matcher():
     return enriched
 
 
-#### Will result in one enriched data set to CSV with proper header and formats to app directory for visualization
+# Will result in one enriched data set to CSV with proper header and formats to app directory for visualization
 def main():
 
     # Return output of match_writer which should be a properly formatted and enriched data set.
@@ -94,6 +96,7 @@ def main():
 
     # Write to CSV in examples directory for visualization
     enriched.to_csv(output, columns = ['Event Time', 'source', 'target', 'src_hit', 'target_hit'],
-                    header = ['Event Time', 'sourceAddress', 'destinationAddress', 'src_hit', 'target_hit'], index=False)
+                    header = ['Event Time', 'sourceAddress', 'destinationAddress', 'src_hit', 'target_hit'], index=False, date_format='%s')
+
 
 main()
